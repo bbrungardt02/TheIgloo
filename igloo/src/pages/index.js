df5c24fe42602b8db43d207ed046636ac685d066
Brandon <bbrungardt02@gmail.com>
2023-08-18 23:26:41 -0500

    layout

--- igloo/src/pages/index.js
+++ igloo/src/pages/index.js
@@ -4,7 +4,7 @@ export const getStaticProps = async () => {
   const products = await prisma.product.findMany({
     where: { stock_quantity: { gt: 0 } }, // Fetch only products with stock
     include: {
-      category: true, // Include the whole category data
+      category: true, // Include the whole category data.
       order_items: true, // Include order items
     },
   });
