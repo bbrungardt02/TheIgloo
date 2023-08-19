ba645b962eaf4d0fc5abe1895c5e73eb095d839f
Brandon <bbrungardt02@gmail.com>
2023-08-19 00:21:42 -0500

    small layout working

--- igloo/src/pages/index.js
+++ igloo/src/pages/index.js
@@ -2,15 +2,25 @@ import prisma from "../lib/prisma";
 
 export const getStaticProps = async () => {
   const products = await prisma.product.findMany({
-    where: { stock_quantity: { gt: 0 } }, // Fetch only products with stock
+    where: { stock_quantity: { gt: 0 } },
     include: {
-      category: true, // Include the whole category data.
-      order_items: true, // Include order items
+      category: true,
+      order_items: true,
     },
   });
 
+  // Convert Date objects to strings
+  const serializableProducts = products.map((product) => ({
+    ...product,
+    created_at: product.created_at.toISOString(), // Convert Date to string
+  }));
+
   return {
-    props: { products },
+    props: { products: serializableProducts }, // Use the modified data
     revalidate: 10,
   };
 };
+
+export default function Home({ products }) {
+  // Your Home component code that uses the fetched products
+}
