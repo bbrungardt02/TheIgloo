ca2173e657152bf6e82cb88c881b6e463ff14176
Brandon <bbrungardt02@gmail.com>
2023-08-22 16:09:52 -0500

    layout

--- igloo/src/lib/prisma.ts
+++ igloo/src/lib/prisma.ts
@@ -67,3 +67,70 @@ export default prisma;
 // Ref: OrderItem.order < Order.order_id
 
 // Ref: OrderItem.product < Product.product_id
+
+// --------------------------------------------------------
+
+// table User {
+//   user_id    Int
+//   username   String
+//   email      String
+//   password   String
+//   created_at DateTime
+//   orders     Order
+// }
+
+// table Product {
+//   product_id     Int
+//   name           String
+//   description    String
+//   price          Float
+//   image_url      String
+//   stock_quantity Int
+//   subcategory_id Int
+//   created_at     DateTime
+//   subcategory    Subcategory
+//   order_items    OrderItem
+// }
+
+// table Subcategory {
+//   subcategory_id Int
+//   name           String
+//   category_id    Int
+//   category       Category
+//   products       Product
+// }
+
+// table Category {
+//   category_id Int
+//   name        String
+//   subcategories Subcategory
+// }
+
+// table Order {
+//   order_id     Int
+//   user_id      Int
+//   order_date   DateTime
+//   total_amount Float
+//   user         User
+//   order_items  OrderItem
+// }
+
+// table OrderItem {
+//   order_item_id Int
+//   order_id      Int
+//   product_id    Int
+//   quantity      Int
+//   subtotal      Float
+//   order         Order
+//   product       Product
+// }
+
+// Ref: Product.subcategory < Subcategory.subcategory_id
+
+// Ref: Subcategory.category < Category.category_id
+
+// Ref: Order.user < User.user_id
+
+// Ref: OrderItem.order < Order.order_id
+
+// Ref: OrderItem.product < Product.product_id
--- igloo/src/pages/Products.js
+++ igloo/src/pages/Products.js
@@ -1,47 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export default function Products() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((json) => {
-        setData(json);
-      })
-      .catch((err) => {
-        console.error("Error fetching data:", err);
-      });
-  }, []);
-
-  // Filter products with category "jewelery"
-  const jeweleryProducts = data.filter(
-    (product) => product.category === "jewelery"
-  );
-
   return (
     <div>
       <p>Diamond grille</p>
-      <div>
-        <h1>Product List</h1>
-
-        {jeweleryProducts.length === 0 ? (
-          <p>No jewelery products available.</p>
-        ) : (
-          <div>
-            {jeweleryProducts.map((product) => (
-              <ul key={product.id}>
-                <li>{product.title}</li>
-                <li>{product.price}</li>
-                <li>{product.category}</li>
-                <li>
-                  <img src={product.image} alt="Item Image" />
-                </li>
-              </ul>
-            ))}
-          </div>
-        )}
-      </div>
     </div>
   );
 }
